5ac0af432460b68f19f787475fa96626462cfae8
Hazl <ynvrshzl@gmail.com>
2026-03-31 18:49:30 -0400

    Add HTML constructor and attributes helper classes
    Implement HTML component construction and attributes management.

---
+++ samples/html.js
@@ -0,0 +1,49 @@
+/**
+ * This is a sample snippet of what HTML constructor code could look like using meta-js
+ * Copyright (c) Hazl, 2026
+*/
+
+/** 
+ * HTML Component construction 🏗️ interface 
+ * @example new HTML("div")
+ */
+export class HTML {
+    constructor(tag) {
+        this.tag = document.createElement(tag)
+    }
+}
+/**
+ * @description HTML Attributes helper
+ */
+export class Attributes {
+    /**
+     * @param {HTMLElement} target is the HTML element for which to modify attributes
+     */
+    constructor(target){
+        this.target = target;
+    }
+    /** The 'css' operation will semantically set inline css for an HTML element. */
+    css(string){
+        this.target.style += string;
+    }
+    /** The 'set' operation will set *any general HTML element attribute, not limited to specific domains. e.g 'css'  */
+    set(argsv){
+        argsv.forEach(([k, v]) => {
+            this.target.setAttribute(k, v);
+        })
+    }
+}
+export class Component extends HTML {
+    constructor(){
+        super();
+    }
+}
+export class Hero {
+
+}
+export class Section {
+    
+}
+export class Article {
+
+}
