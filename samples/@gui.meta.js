85c0872e534d018cc8b88dfb40ca565d601d8f57
Hazl <ynvrshzl@gmail.com>
2026-04-20 11:02:57 -0400

    Create gui.meta.js

---
+++ samples/@gui.meta.js
@@ -0,0 +1,11 @@
+/**
+* @experiment
+* what if we had a visual editor, with components and blocks, but represented in plain-text?
+* a 2Dimensional matrix array, which represents how graphical components are both processed by computers, and assembled by human beings.
+*/
+const baseplate = [
+    [          window        ],
+    [[button, header, button]],
+    [[menubar], [viewport]   ],
+    [ flexgroup              ]
+];
